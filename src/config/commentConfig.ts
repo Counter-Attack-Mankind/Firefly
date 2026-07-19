bd247598319a31b89b0c8265e60b3238a71effbd
Counter-Attack-Mankind <1583295831@qq.com>
2026-07-19 13:14:29 +0800

    评论子域名重新挂载

--- src/config/commentConfig.ts
+++ src/config/commentConfig.ts
@@ -6,7 +6,7 @@ export const commentConfig: CommentConfig = {
 
 	//twikoo评论系统配置，版本1.7.4
 	twikoo: {
-		envId: "https://twikoo-cloudflare.loushengjie527.workers.dev",
+		envId: "https://comments.loushengjie.xyz",
 		// 设置 Twikoo 评论系统语言
 		lang: "zh-CN",
 		// 是否启用文章访问量统计功能
